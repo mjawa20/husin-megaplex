03d9ae66fe9fab6dc60f8a7e0298674f184cda50
sulkhan10 <sulkhangalangsakti@gmail.com>
2022-12-08 18:18:52 +0700

    mendekin sinopsis

--- index.js
+++ index.js
@@ -1,111 +1,113 @@
-let database = [
-    {
-        judul: 'Batman',
-        sutradara:"Matt Reeves",
-        durasi:"176 minutes",
-        bahasa:"inggris",
-        sinopsis:"Di tahun keduanya sebagai Batman (Robert Pattinson), Bruce Wayne berjuang untuk menemukan tempatnya sebagai pemburu kejahatan. Dia menanamkan rasa takut pada mereka yang melakukan tindakan kriminal, akan tetapi dengan metodenya yang selalu main hakim sendiri. Alasannya, karena dia masih belum mendapatkan kepercayaan dari orang-orang, kecuali Letnan James 'Jim' Gordon (Jeffrey Wright) yang mengizinkan Batman menyelidiki TKP bersamanya. Kasus berikutnya yang harus ia hadapi adalah kasus pembunuhan dan korbannya adalah Walikota Don Mitchell Jr (Rupert Penry-Jones). Orang yang dicurigai sebagai dalang dari pembunuhan tersebut adalah seseorang yang menyebut dirinya The Riddler (Paul Dano). Batman pun mulai mengumpulkan petunjuk yang ditinggalkan si pembunuh setelah ia melakukan aksinya. \n Dengan bantuan Selina Kyle alias Zoe Kravitz yang menjadi pelayan di klub bos mafia Carmine Falcone alias John Turturo, Batman bekerja untuk memecahkan misteri sebelum The Riddler dapat mengklaim korban berikutnya. Misteri yang membawanya pada rahasia besar tentang Gotham dan juga tentang keluarganya",
-        image: "/asset/Batman.jpg"
+let database = [{
+        judul: "Batman",
+        sutradara: "Matt Reeves",
+        durasi: "176 minutes",
+        bahasa: "inggris",
+        sinopsis: "Di tahun keduanya sebagai Batman (Robert Pattinson), Bruce Wayne berjuang untuk menemukan tempatnya sebagai pemburu kejahatan. Dia menanamkan rasa takut pada mereka yang melakukan tindakan kriminal, akan tetapi dengan metodenya yang selalu main hakim sendiri.",
+        //   Alasannya, karena dia masih belum mendapatkan kepercayaan dari orang- orang, kecuali Letnan James 'Jim' Gordon(Jeffrey Wright) yang mengizinkan Batman menyelidiki TKP bersamanya.Kasus berikutnya yang harus ia hadapi adalah kasus pembunuhan dan korbannya adalah Walikota Don Mitchell Jr(Rupert Penry - Jones).Orang yang dicurigai sebagai dalang dari pembunuhan tersebut adalah seseorang yang menyebut dirinya The Riddler(Paul Dano).Batman pun mulai mengumpulkan petunjuk yang ditinggalkan si pembunuh setelah ia melakukan aksinya.\n Dengan bantuan Selina Kyle alias Zoe Kravitz yang menjadi pelayan di klub bos mafia Carmine Falcone alias John Turturo, Batman bekerja untuk memecahkan misteri sebelum The Riddler dapat mengklaim korban berikutnya.Misteri yang membawanya pada rahasia besar tentang Gotham dan juga tentang keluarganya
+        // image: "/asset/Batman.jpg",
     },
     {
-        judul: 'Black Panter: Wakanda Forever',
-        sutradara:"Ryan Coogler",
-        durasi:"161 menit",
-        bahasa:"inggris",
+        judul: "Black Panter: Wakanda Forever",
+        sutradara: "Ryan Coogler",
+        durasi: "161 menit",
+        bahasa: "inggris",
         sinopsis: "Black Panther: Wakanda Forever mengisahkan tentang kerajaan Wakanda yang sedang dilanda kesedihan. Mereka harus kehilangan sosok raja mereka yaitu King T'Chala yang telah meninggal dunia. Rakyat Wakanda tidak hanya menghadapi kesedihan, tetapi juga harus berjuang untuk melindungi kerajaan tersebut.",
-        image: "/asset/black panter 2.jpg"
+        image: "/asset/black panter 2.jpg",
     },
     {
-        judul: 'Doctor Strange in the Multiverse of Madness',
-        sutradara:"Sam Raimi",
-        durasi:"126 minutes",
-        bahasa:"inggris",
+        judul: "Doctor Strange in the Multiverse of Madness",
+        sutradara: "Sam Raimi",
+        durasi: "126 minutes",
+        bahasa: "inggris",
         sinopsis: "Doctor Strange 2 mengisahkan perjuangan Stephen Strange (Benedict Cumberbatch) menghadapi berbagai kejadian aneh akibat pintu lintas semesta yang terbuka pasca kejadian di Spider-Man No way Home.",
-        image: "/asset/Doctor of Strange.jpg" 
+        image: "/asset/Doctor of Strange.jpg",
     },
     {
-        judul: 'Godzilla',
-        sutradara:"Michael Dougherty",
-        durasi:"164 minutes",
-        bahasa:"inggris",
-        sinopsis: "Emma Russell adalah seorang ahli paleobiologi untuk organisasi cryptocryptical Monarch, yang mencari dan mempelajari makhluk raksasa yang dikenal sebagai 'Titans'. Dia dan putrinya, Madison, menyaksikan kelahiran larva yang dijuluki Mothra. Emma menenangkan Mothra dengan 'Orca', sebuah perangkat yang mampu memancarkan frekuensi yang dapat menarik atau mengubah perilaku Titan. Sekelompok teroris lingkungan, yang dipimpin oleh Alan Jonah, tiba-tiba menyerang pangkalan dan menculik Emma dan Madison. Mothra melarikan diri dan menjadi kepompong di bawah air terjun. Ilmuwan raja Dr. Ishirō Serizawa dan Dr. Vivienne Graham mendekati Mark Russell, mantan suami Emma dan ayah Madison, untuk melacak mereka. Tim Monarch mengikuti Godzilla ke Antartika, tempat Jonah bermaksud membebaskan Titan berkepala tiga dengan nama sandi 'Monster Zero'. Emma membebaskan dan membangunkan Monster Zero, yang membunuh beberapa anggota Monarch, termasuk Dr. Graham, sebelum bertarung dengan Godzilla. Tim menyadari bahwa Emma tidak diculik oleh para teroris, tetapi diam-diam bekerja dengan mereka. Emma menghubungi mereka dan berpendapat bahwa Titans harus dibangunkan untuk menyembuhkan planet dari kerusakan yang disebabkan manusia.",
-        image:"/asset/godzilla.jpg"
-        
-        },
+        judul: "Godzilla",
+        sutradara: "Michael Dougherty",
+        durasi: "164 minutes",
+        bahasa: "inggris",
+        sinopsis: "Emma Russell adalah seorang ahli paleobiologi untuk organisasi cryptocryptical Monarch, yang mencari dan mempelajari makhluk raksasa yang dikenal sebagai 'Titans'. Dia dan putrinya, Madison, menyaksikan kelahiran larva yang dijuluki Mothra.",
+        /* Emma menenangkan Mothra dengan 'Orca', sebuah perangkat yang mampu memancarkan frekuensi yang dapat menarik atau mengubah perilaku Titan.Sekelompok teroris lingkungan, yang dipimpin oleh Alan Jonah, tiba- tiba menyerang pangkalan dan menculik Emma dan Madison.Mothra melarikan diri dan menjadi kepompong di bawah air terjun.Ilmuwan raja Dr.Ishirō Serizawa dan Dr.Vivienne Graham mendekati Mark Russell, mantan suami Emma dan ayah Madison, untuk melacak mereka.Tim Monarch mengikuti Godzilla ke Antartika, tempat Jonah bermaksud membebaskan Titan berkepala tiga dengan nama sandi 'Monster Zero'.Emma membebaskan dan membangunkan Monster Zero, yang membunuh beberapa anggota Monarch, termasuk Dr.Graham, sebelum bertarung dengan Godzilla.Tim menyadari bahwa Emma tidak diculik oleh para teroris, tetapi diam - diam bekerja dengan mereka.Emma menghubungi mereka dan berpendapat bahwa Titans harus dibangunkan untuk menyembuhkan planet dari kerusakan yang disebabkan manusia.*/
+
+        image: "/asset/godzilla.jpg",
+    },
     {
-        judul: 'Jurassic World Dominion',
-        sutradara:"Colin Trevorrow",
-        durasi:"146 minutes",
-        bahasa:"inggris",
+        judul: "Jurassic World Dominion",
+        sutradara: "Colin Trevorrow",
+        durasi: "146 minutes",
+        bahasa: "inggris",
         sinopsis: "Dilansir dari Jurassic Wiki, Dominion terjadi empat tahun setelah Isla Nublar dihancurkan. Dinosaurus sekarang hidup dan berburu bersama manusia di seluruh dunia. Keseimbangan yang rapuh ini akan membentuk kembali masa depan dan menentukan apakah manusia akan tetap menjadi predator puncak di planet yang sekarang ditempati bersama makhluk paling menakutkan dalam sejarah.",
-        image:"/asset/Jurrasic World.jpg"
+        image: "/asset/Jurrasic World.jpg",
     },
     {
-        judul: 'KKN di Desa Penari',
-        sutradara:"Awi Suryadi",
-        durasi:"130 minutes",
-        bahasa:"indonesia",
+        judul: "KKN di Desa Penari",
+        sutradara: "Awi Suryadi",
+        durasi: "130 minutes",
+        bahasa: "indonesia",
         sinopsis: "KKN di Desa Penari menceritakan berbagai kejadian mistis yang dialami enam mahasiswa ketika menjalani KKN di sebuah desa terpencil. Film ini menceritakan enam mahasiswa yang sedang menjalani Kuliah Kerja Nyata (KKN) di sebuah desa terpencil.",
-        image:"/asset/KKN di Desa Penari.jpg"
+        image: "/asset/KKN di Desa Penari.jpg",
     },
     {
-        judul: 'Garuda di dadaku',
-        sutradara:"Ifa Isfansyah",
-        durasi:"133 minutes",
-        bahasa:"indonesia",
-        sinopsis: "Garuda di Dadaku adalah film Indonesia yang dirilis pada tanggal 18 Juni 2009 yang disutradarai oleh Ifa Isfansyah dan dijadikan ke dalam novel 'Mimpi Sang Garuda' karya Benny Rhamdani. Film ini dibintangi antara lain oleh Emir Mahira, Aldo Tansani, Marsha Aruan, Ikranegara, Maudy Koesnaedi, Ari Sihasale, dan Ramzi. Pembuatan film ini dilakukan di beberapa lokasi di Jakarta, tepatnya di Stadion Utama Gelora Bung Karno, yang merupakan bagian dari kompleks olahraga Gelanggang Olahraga Bung Karno di kawasan Senayan, Jakarta Selatan, untuk pengambilan adegan seleksi pemain Tim Nasional (Timnas) Indonesia U13 dan pertandingan antara Timnas Indonesia U13 melawan Jepang.",
-        image:"/asset/poster garuda di dadaku.jpg"
+        judul: "Garuda di dadaku",
+        sutradara: "Ifa Isfansyah",
+        durasi: "133 minutes",
+        bahasa: "indonesia",
+        sinopsis: "Garuda di Dadaku adalah film Indonesia yang dirilis pada tanggal 18 Juni 2009 yang disutradarai oleh Ifa Isfansyah dan dijadikan ke dalam novel 'Mimpi Sang Garuda' karya Benny Rhamdani.",
+        //   Film ini dibintangi antara lain oleh Emir Mahira, Aldo Tansani, Marsha Aruan, Ikranegara, Maudy Koesnaedi, Ari Sihasale, dan Ramzi.Pembuatan film ini dilakukan di beberapa lokasi di Jakarta, tepatnya di Stadion Utama Gelora Bung Karno, yang merupakan bagian dari kompleks olahraga Gelanggang Olahraga Bung Karno di kawasan Senayan, Jakarta Selatan, untuk pengambilan adegan seleksi pemain Tim Nasional(Timnas) Indonesia U13 dan pertandingan antara Timnas Indonesia U13 melawan Jepang.
+        image: "/asset/poster garuda di dadaku.jpg",
     },
     {
-        judul: 'The Dark Knight',
-        sutradara:"Christopher Nolan",
-        durasi:"152 minutes",
-        bahasa:"inggris",
+        judul: "The Dark Knight",
+        sutradara: "Christopher Nolan",
+        durasi: "152 minutes",
+        bahasa: "inggris",
         sinopsis: "Sebuah bank di Gotham City dirampok oleh sekelompok kriminal. Joker memanipulasi dengan menyuruh mereka saling membunuh satu sama lain agar mendapatkan bagian yang lebih besar. Para perampok itu pun saling membunuh satu sama lain dan hanya menyisakan Joker seorang yang selamat.",
-        image: "/asset/poster the dark knight.jpg"
+        image: "/asset/poster the dark knight.jpg",
     },
     {
-        judul: 'Scream',
-        sutradara:"Matt Bettinelli-Olpin, Tyler Gillett",
-        durasi:"114 minutes",
-        bahasa:"inggris",
+        judul: "Scream",
+        sutradara: "Matt Bettinelli-Olpin, Tyler Gillett",
+        durasi: "114 minutes",
+        bahasa: "inggris",
         sinopsis: "Film Scream 1 berkisah tentang kehidupan seorang pelajar SMA bernama Sidney Prescott (Neve Campbell). Dia mendapat teror setelah kematian ibunya. Tak cuma Sidney, seluruh teman-temannya yang merupakan remaja juga mendapat terror di kota Woodsboro. Teror dimulai dengan telepon yang mengajukan beberapa pertanyaan.",
-        image: "/asset/scream.jpg" 
+        image: "/asset/scream.jpg",
     },
     {
-        judul: 'Sonic the Hedgehog 2',
-        sutradara:"Jeff Fowler",
-        durasi:"122 minutes",
-        bahasa:"inggris",
+        judul: "Sonic the Hedgehog 2",
+        sutradara: "Jeff Fowler",
+        durasi: "122 minutes",
+        bahasa: "inggris",
         sinopsis: "Sonic the Hedgehog 2 adalah film bergenre animasi, aksi, dan petualangan yang bercerita tentang kembalinya Dr. Robotnik ke Bumi dengan pasukan baru.",
-        image: "/asset/sonic.jpg" 
+        image: "/asset/sonic.jpg",
     },
     {
-        judul: 'Sri Asih',
-        sutradara:"Upi",
-        durasi:"135 minutes",
-        bahasa:"indonesia",
-        sinopsis:"Alana tidak mengerti mengapa dia selalu dikuasai oleh kemarahan. Tapi dia selalu berusaha untuk melawannya. Dia lahir saat letusan gunung berapi yang memisahkan dia dan orang tuanya. Dia kemudian diadopsi oleh seorang wanita kaya yang berusaha membantunya menjalani kehidupan normal. Tapi saat dewasa, Alana menemukan kebenaran tentang asalnya. Dia bukan manusia biasa. Dia bisa menjadi kebaikan untuk kehidupan. Atau menjadi kehancuran bila ia tidak dapat mengendalikan amarahnya." ,
-        image: "/asset/Sri Asih.jpg" 
+        judul: "Sri Asih",
+        sutradara: "Upi",
+        durasi: "135 minutes",
+        bahasa: "indonesia",
+        sinopsis: "Alana tidak mengerti mengapa dia selalu dikuasai oleh kemarahan. Tapi dia selalu berusaha untuk melawannya. Dia lahir saat letusan gunung berapi yang memisahkan dia dan orang tuanya.",
+        //   Dia kemudian diadopsi oleh seorang wanita kaya yang berusaha membantunya menjalani kehidupan normal.Tapi saat dewasa, Alana menemukan kebenaran tentang asalnya.Dia bukan manusia biasa.Dia bisa menjadi kebaikan untuk kehidupan.Atau menjadi kehancuran bila ia tidak dapat mengendalikan amarahnya.
+        image: "/asset/Sri Asih.jpg",
     },
     {
-        judul: 'Turning Year',
-        sutradara:"Domee Shi",
-        durasi:"100 minutes",
-        bahasa:"inggris",
+        judul: "Turning Year",
+        sutradara: "Domee Shi",
+        durasi: "100 minutes",
+        bahasa: "inggris",
         sinopsis: "Film Turning Red bercerita tentang seorang gadis berusia 13 tahun yang bernama Meilin “Mei” Lee yang hidup di Toronto. Mei merupakan sosok remaja yang unik dan percaya diri. Ia memiliki tiga orang teman baik. Mei banyak menghabiskan waktu bersama teman-teman dekatnya.",
-        image: "/asset/Turning Year.jpg", 
+        image: "/asset/Turning Year.jpg",
     },
 ]
 
-
-function filterMovie(database){
-    let result=[]
-    let obj={}
-    for(let x=0;x<database.length;x++){
-        if(database[x].bahasa === "indonesia"){
+function filterMovie(database) {
+    let result = []
+    let obj = {}
+    for (let x = 0; x < database.length; x++) {
+        if (database[x].bahasa === "indonesia") {
             result.push("Lokal")
-        }else if(database[x].bahasa === "inggris"){
+        } else if (database[x].bahasa === "inggris") {
             result.push("Mancanegara")
         }
     }
@@ -114,14 +116,17 @@ function filterMovie(database){
 }
 // console.log(megaplex(database))
 
-
-const getFilmByIndex = (database,index) => {
-    if(index === undefined || index >= database.length || index < 0 || typeof index !=="number"){
+const getFilmByIndex = (database, index) => {
+    if (
+        index === undefined ||
+        index >= database.length ||
+        index < 0 ||
+        typeof index !== "number"
+    ) {
         return "mikir"
-    }else{
+    } else {
         return database[index]
     }
 }
 
-console.log(getFilmByIndex(database,7));
-
+console.log(getFilmByIndex(database, 7))
