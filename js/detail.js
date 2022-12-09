44f01be806c7ef496778556e7d027e09035c2bc7
Muhammad Jawahiruzzaman <m.jawahiruzzaman@gmail.com>
2022-12-09 07:31:55 +0700

    add embed youtube link

--- js/detail.js
+++ js/detail.js
@@ -7,6 +7,7 @@ let database = [{
         sinopsis: "Di tahun keduanya sebagai Batman (Robert Pattinson), Bruce Wayne berjuang untuk menemukan tempatnya sebagai pemburu kejahatan. Dia menanamkan rasa takut pada mereka yang melakukan tindakan kriminal, akan tetapi dengan metodenya yang selalu main hakim sendiri.",
         //  Alasannya, karena dia masih belum mendapatkan kepercayaan dari orang- orang, kecuali Letnan James 'Jim' Gordon(Jeffrey Wright) yang mengizinkan Batman menyelidiki TKP bersamanya.Kasus berikutnya yang harus ia hadapi adalah kasus pembunuhan dan korbannya adalah Walikota Don Mitchell Jr(Rupert Penry - Jones).Orang yang dicurigai sebagai dalang dari pembunuhan tersebut adalah seseorang yang menyebut dirinya The Riddler(Paul Dano).Batman pun mulai mengumpulkan petunjuk yang ditinggalkan si pembunuh setelah ia melakukan aksinya.\n Dengan bantuan Selina Kyle alias Zoe Kravitz yang menjadi pelayan di klub bos mafia Carmine Falcone alias John Turturo, Batman bekerja untuk memecahkan misteri sebelum The Riddler dapat mengklaim korban berikutnya.Misteri yang membawanya pada rahasia besar tentang Gotham dan juga tentang keluarganya"
         image: "asset/Batman.jpg",
+        trailer: "mqqft2x_Aa4"
     },
     {
         id: 2,
@@ -16,6 +17,7 @@ let database = [{
         bahasa: "inggris",
         sinopsis: "Black Panther: Wakanda Forever mengisahkan tentang kerajaan Wakanda yang sedang dilanda kesedihan. Mereka harus kehilangan sosok raja mereka yaitu King T'Chala yang telah meninggal dunia. Rakyat Wakanda tidak hanya menghadapi kesedihan, tetapi juga harus berjuang untuk melindungi kerajaan tersebut.",
         image: "asset/black panter 2.jpg",
+        trailer: "_Z3QKkl1WyM"
     },
     {
         id: 3,
@@ -25,6 +27,7 @@ let database = [{
         bahasa: "inggris",
         sinopsis: "Doctor Strange 2 mengisahkan perjuangan Stephen Strange (Benedict Cumberbatch) menghadapi berbagai kejadian aneh akibat pintu lintas semesta yang terbuka pasca kejadian di Spider-Man No way Home.",
         image: "asset/Doctor of Strange.jpg",
+        trailer: "aWzlQ2N6qqg"
     },
     {
         id: 4,
@@ -35,6 +38,7 @@ let database = [{
         sinopsis: "Emma Russell adalah seorang ahli paleobiologi untuk organisasi cryptocryptical Monarch, yang mencari dan mempelajari makhluk raksasa yang dikenal sebagai 'Titans'. Dia dan putrinya, Madison, menyaksikan kelahiran larva yang dijuluki Mothra.",
         // Emma menenangkan Mothra dengan 'Orca', sebuah perangkat yang mampu melocalDatarkan frekuensi yang dapat menarik atau mengubah perilaku Titan.Sekelompok teroris lingkungan, yang dipimpin oleh Alan Jonah, tiba- tiba menyerang pangkalan dan menculik Emma dan Madison.Mothra melarikan diri dan menjadi kepompong di bawah air terjun.Ilmuwan raja Dr.Ishirō Serizawa dan Dr.Vivienne Graham mendekati Mark Russell, mantan suami Emma dan ayah Madison, untuk melacak mereka.Tim Monarch mengikuti Godzilla ke Antartika, tempat Jonah bermaksud membebaskan Titan berkepala tiga dengan nama sandi 'Monster Zero'.Emma membebaskan dan membangunkan Monster Zero, yang membunuh beberapa anggota Monarch, termasuk Dr.Graham, sebelum bertarung dengan Godzilla.Tim menyadari bahwa Emma tidak diculik oleh para teroris, tetapi diam - diam bekerja dengan mereka.Emma menghubungi mereka dan berpendapat bahwa Titans harus dibangunkan untuk menyembuhkan planet dari kerusakan yang disebabkan manusia."
         image: "asset/godzilla.jpg",
+        trailer: "odM92ap8_c0"
     },
     {
         id: 5,
@@ -44,6 +48,7 @@ let database = [{
         bahasa: "inggris",
         sinopsis: "Dilansir dari Jurassic Wiki, Dominion terjadi empat tahun setelah Isla Nublar dihancurkan. Dinosaurus sekarang hidup dan berburu bersama manusia di seluruh dunia. Keseimbangan yang rapuh ini akan membentuk kembali masa depan dan menentukan apakah manusia akan tetap menjadi predator puncak di planet yang sekarang ditempati bersama makhluk paling menakutkan dalam sejarah.",
         image: "asset/Jurrasic World.jpg",
+        trailer: "fb5ELWi-ekk"
     },
     {
         id: 6,
@@ -53,6 +58,7 @@ let database = [{
         bahasa: "indonesia",
         sinopsis: "KKN di Desa Penari menceritakan berbagai kejadian mistis yang dialami enam mahasiswa ketika menjalani KKN di sebuah desa terpencil. Film ini menceritakan enam mahasiswa yang sedang menjalani Kuliah Kerja Nyata (KKN) di sebuah desa terpencil.",
         image: "asset/KKN di Desa Penari.jpg",
+        trailer: "PAMx9m4Z2V4"
     },
     {
         id: 7,
@@ -63,6 +69,7 @@ let database = [{
         sinopsis: "Garuda di Dadaku adalah film Indonesia yang dirilis pada tanggal 18 Juni 2009 yang disutradarai oleh Ifa Isfansyah dan dijadikan ke dalam novel 'Mimpi Sang Garuda' karya Benny Rhamdani. ",
         // Film ini dibintangi antara lain oleh Emir Mahira, Aldo Tansani, Marsha Aruan, Ikranegara, Maudy Koesnaedi, Ari Sihasale, dan Ramzi.Pembuatan film ini dilakukan di beberapa lokasi di Jakarta, tepatnya di Stadion Utama Gelora Bung Karno, yang merupakan bagian dari kompleks olahraga Gelanggang Olahraga Bung Karno di kawasan Senayan, Jakarta Selatan, untuk pengambilan adegan seleksi pemain Tim Nasional(Timnas) Indonesia U13 dan pertandingan antara Timnas Indonesia U13 melawan Jepang."
         image: "asset/poster garuda di dadaku.jpg",
+        trailer: "aPY9VM9-AE4"
     },
     {
         id: 8,
@@ -72,6 +79,7 @@ let database = [{
         bahasa: "inggris",
         sinopsis: "Sebuah bank di Gotham City dirampok oleh sekelompok kriminal. Joker memanipulasi dengan menyuruh mereka saling membunuh satu sama lain agar mendapatkan bagian yang lebih besar. Para perampok itu pun saling membunuh satu sama lain dan hanya menyisakan Joker seorang yang selamat.",
         image: "asset/poster the dark knight.jpg",
+        trailer: "EXeTwQWrcwY"
     },
     {
         id: 9,
@@ -81,6 +89,7 @@ let database = [{
         bahasa: "inggris",
         sinopsis: "Film Scream 1 berkisah tentang kehidupan seorang pelajar SMA bernama Sidney Prescott (Neve Campbell). Dia mendapat teror setelah kematian ibunya. Tak cuma Sidney, seluruh teman-temannya yang merupakan remaja juga mendapat terror di kota Woodsboro. Teror dimulai dengan telepon yang mengajukan beberapa pertanyaan.",
         image: "asset/scream.jpg",
+        trailer: "beToTslH17s"
     },
     {
         id: 10,
@@ -90,6 +99,7 @@ let database = [{
         bahasa: "inggris",
         sinopsis: "Sonic the Hedgehog 2 adalah film bergenre animasi, aksi, dan petualangan yang bercerita tentang kembalinya Dr. Robotnik ke Bumi dengan pasukan baru.",
         image: "asset/sonic.jpg",
+        trailer: "47r8FXYZWNU"
     },
     {
         id: 11,
@@ -100,6 +110,7 @@ let database = [{
         sinopsis: "Alana tidak mengerti mengapa dia selalu dikuasai oleh kemarahan. Tapi dia selalu berusaha untuk melawannya. Dia lahir saat letusan gunung berapi yang memisahkan dia dan orang tuanya.",
         //  Dia kemudian diadopsi oleh seorang wanita kaya yang berusaha membantunya menjalani kehidupan normal.Tapi saat dewasa, Alana menemukan kebenaran tentang asalnya.Dia bukan manusia biasa.Dia bisa menjadi kebaikan untuk kehidupan.Atau menjadi kehancuran bila ia tidak dapat mengendalikan amarahnya."
         image: "asset/Sri Asih.jpg",
+        trailer: "564eG_1Mvf0"
     },
     {
         id: 12,
@@ -109,6 +120,7 @@ let database = [{
         bahasa: "inggris",
         sinopsis: "Film Turning Red bercerita tentang seorang gadis berusia 13 tahun yang bernama Meilin “Mei” Lee yang hidup di Toronto. Mei merupakan sosok remaja yang unik dan percaya diri. Ia memiliki tiga orang teman baik. Mei banyak menghabiskan waktu bersama teman-teman dekatnya.",
         image: "asset/Turning Year.jpg",
+        trailer: "XdKzUbAiswE"
     },
 ]
 
@@ -163,6 +175,7 @@ function render() {
     let id = getId()
 
     let detail = document.getElementById("detail")
+    let embed = document.getElementById("embed")
     let data = getFilmById(database, id)
 
     detail.innerHTML = `
@@ -189,6 +202,12 @@ function render() {
           </div>
       </div>
   </div>`
+
+  embed.innerHTML = `
+  <iframe style="width: 100%; height: 400px;" src="https://www.youtube.com/embed/${data.trailer}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  `
+
+
 }
 
 render()
